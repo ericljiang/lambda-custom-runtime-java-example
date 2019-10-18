d414b3d51410f0b5b60452a9a1bff08bff291803
Eric Jiang <ejiang@amazon.com>
2019-10-17 22:37:07 -0700

    Add logging.

--- src/main/java/lambda/custom/runtime/java/example/App.java
+++ src/main/java/lambda/custom/runtime/java/example/App.java
@@ -3,12 +3,15 @@
  */
 package lambda.custom.runtime.java.example;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        log.info(new App().getGreeting());
     }
 }
--- src/main/java/lambda/custom/runtime/java/example/Handler.java
+++ src/main/java/lambda/custom/runtime/java/example/Handler.java
@@ -3,11 +3,17 @@ package lambda.custom.runtime.java.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Handler implements RequestHandler<Request, Response> {
+
     @Override
     public Response handleRequest(Request input, Context context) {
+        log.info("Received request.");
         return Response.builder()
                 .message("Hello world")
                 .build();
     }
+
 }
